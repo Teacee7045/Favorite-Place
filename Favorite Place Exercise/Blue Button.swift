20e497a778530f3279dd2ccf4c42ff21f4054fe8
Thomas Chow <Kirthmas@kirthmas-mbp.home>
2016-01-23 01:25:05 +0100

    first commit

---
+++ Favorite Place Exercise/Blue Button.swift
@@ -0,0 +1,21 @@
+//
+//  Blue Button.swift
+//  Favorite Place Exercise
+//
+//  Created by Thomas Chow on 22/01/2016.
+//  Copyright © 2016 Paws. All rights reserved.
+//
+
+import UIKit
+
+class Blue_Button: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Favorite-Place >> README.me
+git init
+git add README.md
+git commit -m first commit
+git remote add origin git@github.com:Teacee7045/Favorite-Place.git
+git push -u origin master
+
+
+
+
+git remote add origin git@github.com:Teacee7045/Favorite-Place.git
+git push -u origin master
+echo # Favorite-Place
